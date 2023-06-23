41e40ba415b7a10271f15890a61cf9ef5add700c
Jared Meyers <78173431+jaredmeyers@users.noreply.github.com>
2023-06-23 13:54:28 -0400

    Update secondary_analysis.R

--- script/secondary_analysis.R
+++ script/secondary_analysis.R
@@ -2,14 +2,32 @@ library(stringr)
 library(BSgenome, lib.loc = "/appl/R-4.2/lib64/R/library")
 library(BSgenome.Mmulatta.UCSC.rheMac10)
 
-file_C592 <- read_csv("file_C592.csv")
+wd <- unlist(str_split(getwd(), '/'))
+parent_dir <- paste0(wd[2:length(wd)-1], collapse = '/')
+
+bams <- list.files(path = '../', pattern = '.*_ref_map_filtered_sorted_dedup_novec.bam$')
+abspath_bams <- paste(parent_dir, bams, sep = '/')
+
+beds <- list.files(path = '../', pattern = '.*_ref_peak.bed$')
+abspath_beds <- paste(parent_dir, beds, sep = '/') 
+
+annos <- list.files(path = '../', pattern = '.*_ref_peak_anno.tsv$')
+abspath_annos <- paste(parent_dir, annos, sep = '/') 
+
+name_idx <- mapply(lcprefix, bams, beds) - 5
+ids <- substr(bams, 1, name_idx)
+
+file_paths <- data.frame(bam = abspath_bams, bed = abspath_beds, anno = abspath_annos, row.names = ids)
+write.csv(file_paths, 'filepaths.csv')
+
+#file_C592 <- read_csv("file_C592.csv")
 
 ########################
 # Mispriming
 
 seed.seq <- "ACTCCCTC"
 
-mispriming <- mapply(CallMispriming, fbam = file_C592$bam, fbed = file_C592$bed, seed.seq = seed.seq)
+mispriming <- mapply(CallMispriming, fbam = file_paths$bam, fbed = file_paths$bed, seed.seq = seed.seq)
 mispriming_list <- list()
 for (i in 1:length(mispriming)){
   temp <- data.frame(mispriming[i])
@@ -27,19 +45,27 @@ for (i in 1:length(mispriming)){
 ##############################
 # Annotation
 
-anno <- mapply(ReformatPeakAnno, file_C592$anno)
+anno <- mapply(ReformatPeakAnno, file_paths$anno)
 anno_list <- list()
 smp_names <- list()
 for (i in 1:length(anno)){
   temp <- data.frame(anno[i])
   tryCatch(
     expr={
-  colnames(temp) <- c("Peak", "Chrom", "Start", "End", "Read", "Score", "Gene_ID", "Gene_Name", "Gene_Biotype", "Gene_Description", "Gene_TSS", "Gene_TSS2", "Strand", "Overlap", "Dist2TSS", "CDS", "Promotor", "5UTR", "3UTR", "Intragenic", "Intergenic", "Representative")
+      colnames(temp) <- c("Peak", "Chrom", "Start", "End", "Read", "Score", "Gene_ID", "Gene_Name", "Gene_Biotype", "Gene_Description", "Gene_TSS", "Gene_TSS2", "Strand", "Overlap", "Dist2TSS", "CDS", "Promotor", "5UTR", "3UTR", "Intragenic", "Intergenic", "Representative")
     },
-  error = function(e){})
+    error = function(e){})
+  
+  
+  smp_names[[i]] <- str_sub(unlist(strsplit(names(anno[i]), '/'))[6], 1, -19)
+  
+  tryCatch(
+    expr={
+      temp$sample <- smp_names[[i]]    },
+    error = function(e){})
   
   anno_list[[i]] <- temp
-  smp_names[[i]] <- str_sub(unlist(strsplit(names(anno[i]), '/'))[6], 1, -19)
+  
   write.csv(temp, paste('annotation/',str_sub(unlist(strsplit(names(anno[i]), '/'))[6], 1, -19),'_anno.csv', sep=''))
 }
 #anno <- lapply(anno , function(x){ row.names(x)<-as.character(x$Peak); x}) #set the peak to rownames
@@ -54,16 +80,15 @@ for (i in 1:length(mispriming_list)){
   masked_anno <- anno_list[[i]][!mispriming_list[[i]][['Mispriming']],] # only keeps FALSE in mispriming
   tryCatch(
     exp = {
-    match <- MatchGenomeToPWM(Mmulatta, paste0('chr', masked_anno$Chrom), masked_anno$Start, masked_anno$End, pwm)
-    masked_anno$PWM_Score <- match$Score
-    write.csv(masked_anno, paste0('PWM_match/', smp_names[[i]], '.csv'))
+      match <- MatchGenomeToPWM(Mmulatta, paste0('chr', masked_anno$Chrom), masked_anno$Start, masked_anno$End, pwm)
+      masked_anno$PWM_Score <- match$Score
+      write.csv(masked_anno, paste0('PWM_match/', smp_names[[i]], '.csv'))
     },
     error = function(e){ 
       print(paste(smp_names[[i]], 'has no non-mispriming peaks'))
     }
   )
 }
-
 
 ####################
 # Concat result dataframes
@@ -81,16 +106,18 @@ score_dfs_fixed <- lapply(score_dfs, function(df) {
   transform(df, Chrom = as.character(Chrom))})
 combined <- bind_rows(score_dfs_fixed)
 
-tempbed <- data.frame(chr=combined$Chrom, start=combined$Start, end=combined$End, name=rownames(combined), score=combined$Score, strand=combined$Strand)
-merge_bed<- bedr.merge.region(tempbed, check.chr = FALSE, list.names = TRUE)
+tempbed <- data.frame(chr=combined$Chrom, start=combined$Start, end=combined$End, name=combined$sample, score=combined$Score, strand=combined$Strand)
 
-no_overlap_bed <- merge_bed[!grepl(",", merge_bed$names), ]
-filtered_merged_bed <- merge_bed[grepl(",", merge_bed$names), ] #only the peaks that have overlap
-
-split_names <- lapply(filtered_merged_bed$names, function(x) {
-  lapply(strsplit(x, ",")[[1]], function(y){as.integer(y)})
-})
-
-a<-combined[unlist(split_names),]
-
-b <- bind_rows(no_overlap_bed, filtered_merged_bed)
+write_tsv(tempbed, '2020-007-liver-no-mispriming-peaks.bed')
+# merge_bed<- bedr.merge.region(tempbed, check.chr = FALSE, list.names = TRUE)
+# 
+# no_overlap_bed <- merge_bed[!grepl(",", merge_bed$names), ]
+# filtered_merged_bed <- merge_bed[grepl(",", merge_bed$names), ] #only the peaks that have overlap
+# 
+# split_names <- lapply(filtered_merged_bed$names, function(x) {
+#   lapply(strsplit(x, ",")[[1]], function(y){as.integer(y)})
+# })
+# 
+# a<-combined[unlist(split_names),]
+# 
+# b <- bind_rows(no_overlap_bed, filtered_merged_bed)
