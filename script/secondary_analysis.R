e2524580852633f6ff018e7dd7f47c573cf1a774
Jared Meyers <78173431+jaredmeyers@users.noreply.github.com>
2023-07-26 10:19:42 -0400

    Update secondary_analysis.R

--- script/secondary_analysis.R
+++ script/secondary_analysis.R
@@ -1,4 +1,5 @@
 library(stringr)
+library(tidyverse)
 library(BSgenome, lib.loc = "/appl/R-4.2/lib64/R/library")
 library(BSgenome.Mmulatta.UCSC.rheMac10)
 
@@ -26,8 +27,8 @@ write.csv(file_paths, 'filepaths.csv')
 
 library(bedtoolsr)
 
-off_target_beds <- mapply(bt.window, a=file_paths$bed, b='ARCUS_target_Mmul10.bed', w=500, v=TRUE, SIMPLIFY = FALSE)
-
+off_target_beds <- mapply(bt.window, a=file_paths$bed, b='../ARCUS_target_Mmul10.bed', w=500, v=TRUE, SIMPLIFY = FALSE)
+dir.create('offtarget_beds')
 for (i in 1:length(off_target_beds)){
   write_tsv(data.frame(off_target_beds[i]), paste0('offtarget_beds/offtarget_', unlist(strsplit(names(off_target_beds[i]), '/'))[6]), col_names = FALSE)
 }
@@ -37,6 +38,7 @@ for (i in 1:length(off_target_beds)){
 
 
 seed.seq <- "ACTCCCTC"
+dir.create('mispriming')
 dir.create('mispriming/bed')
 mispriming <- mapply(CallMispriming, fbam = file_paths$bam, fbed = paste0( 'offtarget_beds/' ,list.files(path = 'offtarget_beds', pattern = '.*bed$')), seed.seq = seed.seq)
 
@@ -56,22 +58,23 @@ for (i in 1:length(mispriming)){
   name <- str_sub(unlist(strsplit(names(mispriming[i]), '/'))[6], 1, -41)
   
   print(name)
+  
   file.create(paste0('mispriming/bed/', name, 'offtarget_no_mispriming.bed'))
   
   if (nrow(realpeaks) > 0){
-  for (j in 1:nrow(realpeaks)){
-    chrom <- strsplit(rownames(realpeaks), ':')[[j]][1]
-    
-    coords <- strsplit(rownames(realpeaks), ':')[[j]][2]
-    start <- strsplit(coords, '-')[[1]][1]
-    end <- strsplit(coords, '-')[[1]][2]
-    
-    df <- data.frame(chrom=chrom, start=start, end=end, name=name, score=temp$Score[j])
-    write_tsv(df, paste0('mispriming/bed/', name, 'offtarget_no_mispriming.bed'), append = TRUE)
-  }
+    for (j in 1:nrow(realpeaks)){
+      chrom <- strsplit(rownames(realpeaks), ':')[[j]][1]
+      
+      coords <- strsplit(rownames(realpeaks), ':')[[j]][2]
+      start <- strsplit(coords, '-')[[1]][1]
+      end <- strsplit(coords, '-')[[1]][2]
+      
+      df <- data.frame(chrom=chrom, start=start, end=end, name=name, score=temp$Score[j])
+      write_tsv(df, paste0('mispriming/bed/', name, 'offtarget_no_mispriming.bed'), append = TRUE)
+    }
   }
   write.csv(temp, paste('mispriming/',name ,'_mispriming.csv', sep=''))
-  }
+}
 
 
 
@@ -79,8 +82,8 @@ for (i in 1:length(mispriming)){
 
 ##############################
 # Annotation
-
-anno <- mapply(ReformatPeakAnno, file_paths$anno)
+dir.create('annotation')
+anno <- mapply(ReformatPeakAnno, file_paths$anno, SIMPLIFY = FALSE)
 anno_list <- list()
 smp_names <- list()
 for (i in 1:length(anno)){
@@ -89,9 +92,9 @@ for (i in 1:length(anno)){
     expr={
       colnames(temp) <- c("Peak", "Chrom", "Start", "End", "Read", "Score", "Gene_ID", "Gene_Name", "Gene_Biotype", "Gene_Description", "Gene_TSS", "Gene_TTS", "Strand", "Overlap", "Dist2TSS", "CDS", "Promoter", "5UTR", "3UTR", "Intragenic", "Intergenic", "Representative")
       
-      },
+    },
     error = function(e){})
-  temp <- temp[!(temp$Chrom == '1' & temp$Start >= 169386114 -1000 & temp$End <= 169386136 + 1000), ] #remove on-target
+  temp <- temp[!(temp$Chrom == '1' & temp$Start >= 169386114 -500 & temp$End <= 169386136 + 500), ] #remove on-target
   temp <- temp[mispriming_list[[i]]$Mispriming == FALSE,] #remove mispriming
   smp_names[[i]] <- str_sub(unlist(strsplit(names(anno[i]), '/'))[6], 1, -19)
   
@@ -101,15 +104,12 @@ for (i in 1:length(anno)){
     error = function(e){})
   anno_list[[i]] <- temp
   write.csv(temp, paste('annotation/',str_sub(unlist(strsplit(names(anno[i]), '/'))[6], 1, -19),'_anno.csv', sep=''))
-
-  
-  
-  }
+}
 #anno <- lapply(anno , function(x){ row.names(x)<-as.character(x$Peak); x}) #set the peak to rownames
 
 ###################
 # Match to PWM
-
+dir.create('PWM_match')
 pwm <- as.matrix(readRDS('PWM.rds'))
 for (i in 1:length(mispriming_list)){
   print(i)
@@ -146,7 +146,7 @@ combined <- bind_rows(score_dfs_fixed)
 tempbed <- data.frame(chr=combined$Chrom, start=combined$Start, end=combined$End, name=combined$sample, score=combined$Score, strand=combined$Strand)
 
 
-out_name <- '2020-007-nonliver-no-mispriming-peaks.bed'
+out_name <- 'C662-offtarget-no-mispriming-peaks.bed'
 write_tsv(tempbed, out_name, col_names = FALSE)
 out.sort <- bt.sort(out_name)
 out.merged <- bt.merge(out.sort, d=44, c='4,5,6', o='collapse,sum,distinct')
